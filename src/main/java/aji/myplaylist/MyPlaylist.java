32b7247c0fb351c902b9cfdf926125683471fc4c
aji <2442733838@qq.com>
2025-10-16 15:40:59 +0800

    初始提交

---
+++ src/main/java/aji/myplaylist/MyPlaylist.java
@@ -0,0 +1,15 @@
+package aji.myplaylist;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyPlaylist implements ModInitializer {
+	public static final String MOD_ID = "myplaylist";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
