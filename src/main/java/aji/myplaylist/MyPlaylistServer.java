56252087bcabfff6d72fc4b7b56fc3097cd7f4b8
aji <2442733838@qq.com>
2025-10-16 18:08:52 +0800

    添加服务器入口点

---
+++ src/main/java/aji/myplaylist/MyPlaylistServer.java
@@ -0,0 +1,10 @@
+package aji.myplaylist;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class MyPlaylistServer implements DedicatedServerModInitializer {
+    @Override
+    public void onInitializeServer() {
+
+    }
+}
