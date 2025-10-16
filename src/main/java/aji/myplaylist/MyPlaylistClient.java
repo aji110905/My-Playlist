666e07c22be3283439336823344988c402d4cb59
aji <2442733838@qq.com>
2025-10-16 15:43:48 +0800

    添加客户端入口点

---
+++ src/main/java/aji/myplaylist/MyPlaylistClient.java
@@ -0,0 +1,9 @@
+package aji.myplaylist;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyPlaylistClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
