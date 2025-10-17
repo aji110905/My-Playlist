9882e3a3e7b971714fee5d6189dc83c69c9523fe
aji <2442733838@qq.com>
2025-10-17 15:22:23 +0800

    添加读取并解析mp3文件的类

--- src/main/java/aji/myplaylist/MyPlaylist.java
+++ src/main/java/aji/myplaylist/MyPlaylist.java
@@ -1,5 +1,6 @@
 package aji.myplaylist;
 
+import aji.myplaylist.music.Musics;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 
@@ -10,8 +11,10 @@ public class MyPlaylist implements ModInitializer {
 	public static final String MOD_ID = "myplaylist";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     public static EnvType Env = null;
+    public static Musics musics = null;
 
 	@Override
 	public void onInitialize() {
-	}
+        musics = new Musics();
+    }
 }
---
+++ src/main/java/aji/myplaylist/music/Musics.java
@@ -0,0 +1,38 @@
+package aji.myplaylist.music;
+
+import aji.myplaylist.MyPlaylist;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Musics {
+    public File playlistFile = new File(FabricLoader.getInstance().getGameDir().toFile(), "playlist");
+    private List<Music> musics = new ArrayList<>();
+
+    public Musics(){
+        playlistFile.mkdirs();
+        File[] files = playlistFile.listFiles();
+        if(files == null) return;
+        for(File file : files){
+            try {
+                musics.add(new Music(file));
+            } catch (IOException e) {
+                MyPlaylist.LOGGER.error("Failed to load music: {}", file.getName(), e);
+            }
+        }
+    }
+
+    public List<Music> getMusics() {
+        return musics;
+    }
+
+    public Music getMusic(String title) {
+        for (Music music : musics) {
+            if (music.getTitle().equals(title)) return music;
+        }
+        return null;
+    }
+}
