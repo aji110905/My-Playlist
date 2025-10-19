695cdd50ef928fe149e2708a89ab7f8498361633
aji <2442733838@qq.com>
2025-10-19 16:00:03 +0800

    使用Minecraft原始的翻译系统

--- src/main/java/aji/myplaylist/client/MyPlaylistClient.java
+++ src/main/java/aji/myplaylist/client/MyPlaylistClient.java
@@ -1,13 +1,10 @@
 package aji.myplaylist.client;
 
-import aji.myplaylist.language.MyPlaylistLanguage;
 import aji.myplaylist.MyPlaylist;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 
 public class MyPlaylistClient implements ClientModInitializer {
-    public static final MyPlaylistLanguage LANGUAGE = new MyPlaylistLanguage();
-
     @Override
     public void onInitializeClient() {
         MyPlaylist.Env = EnvType.CLIENT;
--- src/main/java/aji/myplaylist/server/MyPlaylistServer.java
+++ src/main/java/aji/myplaylist/server/MyPlaylistServer.java
@@ -1,13 +1,10 @@
 package aji.myplaylist.server;
 
-import aji.myplaylist.language.MyPlaylistLanguage;
 import aji.myplaylist.MyPlaylist;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
 
 public class MyPlaylistServer implements DedicatedServerModInitializer {
-    public static final MyPlaylistLanguage LANGUAGE = new MyPlaylistLanguage();
-
     @Override
     public void onInitializeServer() {
         MyPlaylist.Env = EnvType.SERVER;
