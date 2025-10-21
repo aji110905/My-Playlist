861078abaa21441499611a4dea927aebed9e2863
aji <2442733838@qq.com>
2025-10-21 22:20:58 +0800

    添加配置管理器

---
+++ src/main/java/aji/myplaylist/client/config/Configmanager.java
@@ -0,0 +1,20 @@
+package aji.myplaylist.client.config;
+
+/**
+ * 配置管理器
+ */
+public class Configmanager {
+    /**
+     * 从配置文件加载配置
+     */
+    public static void load(){
+
+    }
+
+    /**
+     * 将配置保存到配置文件
+     */
+    public static void save(){
+
+    }
+}
