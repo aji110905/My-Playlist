85c361b6f778fae2305a296fd64239a4a5f5f77e
aji <2442733838@qq.com>
2025-10-30 16:09:52 +0800

    删除关于选项

--- src/main/java/aji/myplaylist/client/gui/screen/OptionsScreen.java
+++ src/main/java/aji/myplaylist/client/gui/screen/OptionsScreen.java
@@ -2,10 +2,9 @@ package aji.myplaylist.client.gui.screen;
 
 import aji.myplaylist.client.MyPlaylistClient;
 import net.minecraft.client.gui.Element;
-import net.minecraft.client.gui.screen.ConfirmLinkScreen;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.tooltip.Tooltip;
 import net.minecraft.client.gui.widget.*;
-import net.minecraft.client.option.SimpleOption;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
 
@@ -36,8 +35,7 @@ public class OptionsScreen extends Screen {
         DirectionalLayoutWidget directionalLayoutWidget1 = this.layout.addHeader(DirectionalLayoutWidget.vertical().spacing(8));
         directionalLayoutWidget1.add(new TextWidget(Text.translatable("myplaylist.screen.options"), this.textRenderer), Positioner::alignHorizontalCenter);
         DirectionalLayoutWidget directionalLayoutWidget2 = directionalLayoutWidget1.add(DirectionalLayoutWidget.horizontal()).spacing(8);
-        directionalLayoutWidget2.add(ButtonWidget.builder(Text.translatable("myplaylist.screen.playlist"), button -> this.client.setScreen(new PlaylistScreen(this))).width(150).build());
-        directionalLayoutWidget2.add(ButtonWidget.builder(Text.translatable("myplaylist.screen.options.about"), ConfirmLinkScreen.opening(this, "https://github.com/aji110905/My-Playlist")).width(150).build());
+        directionalLayoutWidget2.add(ButtonWidget.builder(Text.translatable("myplaylist.screen.playlist"), button -> this.client.setScreen(new PlaylistScreen(this))).width(308).build());
 
         GridWidget gridWidget = new GridWidget();
         gridWidget.getMainPositioner().marginX(4).marginBottom(4).alignHorizontalCenter();
